93ce636144d937497b719af05beebc1ae4b04abd
Alejandro Ulate <alejandro.ulate@buildto.io>
2019-06-21 19:08:58 -0600

    I2C with driver done!

--- app/src/main/java/cr/codingale/playground/MainActivity.java
+++ app/src/main/java/cr/codingale/playground/MainActivity.java
@@ -15,17 +15,13 @@ import com.google.android.things.pio.Pwm;
 import java.io.IOException;
 import java.util.List;
 
+import nz.geek.android.things.drivers.adc.I2cAdc;
+
 public class MainActivity extends Activity {
     public static final String TAG = "Android Things Playground:";
-    private static final byte OUT_ACTIVE = 0x40;
-    private static final byte AUTOINCREMENT = 0X04;
-    private static final byte IN_0 = 0X00;
-    private static final byte IN_1 = 0x01;
-    private static final byte IN_2 = 0x02;
-    private static final byte IN_3 = 0x03;
-    private static String IN_I2C_NAME = "I2C1";
-    private static final int IN_I2C_DIRECTION = 0x48;
-    private I2cDevice i2c;
+    private I2cAdc adc;
+    private Handler handler = new Handler();
+    private Runnable runnable = new UpdateRunner();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,38 +35,26 @@ public class MainActivity extends Activity {
     }
 
     private void setupI2C() {
-        try {
-            i2c = PeripheralManager.getInstance().openI2cDevice(IN_I2C_NAME, IN_I2C_DIRECTION);
-
-            byte[] config = new byte[2];
-            config[0] = OUT_ACTIVE + AUTOINCREMENT;
-            config[1] = (byte) 0x80;
-            i2c.write(config, config.length);
-
-            byte[] buffer = new byte[5];
-            i2c.read(buffer, buffer.length);
-
-
-            String s = "";
-            for (int i = 0; i < buffer.length; i++) {
-                s += " byte"+i+": "+ (buffer[i]&0xFF);
-
-            }
-            Log.d(TAG, s);
-        } catch (IOException e) {
-            Log.e(TAG, "setupI2C:", e);
-        }
+        I2cAdc.I2cAdcBuilder builder =  I2cAdc.builder();
+        adc = builder.address(0).fourSingleEnded().withConversionRate(100).build();
+        adc.startConversions();
+        handler.post(runnable);
     }
 
     @Override
     protected void onDestroy() {
-        try {
-            i2c.close();
-            i2c = null;
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
         super.onDestroy();
+    }
+
+    private class UpdateRunner implements Runnable {
+        @Override
+        public void run() {
+            String s = "";
+            for (int i = 0; i < 4; i++) {
+                s += " canal " + i + adc.readChannel(i);
+            }
+            Log.d(TAG, s);
+            handler.postDelayed(this, 1000);
+        }
     }
 }
